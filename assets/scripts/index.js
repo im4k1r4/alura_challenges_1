5109aa5d448d87a7973053799eb036f94b6c721a
Thiago Andrade <imthiagomartins@live.com>
2024-04-29 22:32:24 -0300

    Aplicando possibilidade de alterar a cor de fundo do editor

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,8 @@
 const language = document.querySelector('#linguagem')
 const codeArea = document.querySelector('.editor-area-container')
 const button = document.querySelector('.highlight-button')
+const editorBg = document.querySelector('.editor-area-total')
+var colorEditor = document.querySelector('.editor-color')
 
 function applyHighlight() {
     const code = codeArea.innerText
@@ -17,6 +19,15 @@ function applyHighlight() {
     hljs.highlightElement(codeArea.querySelector('code'))
 }
 
+function selectedColor() {
+    var actualColor = colorEditor.value
+    editorBg.style.backgroundColor = actualColor
+}
+
 button.addEventListener('click', () => {
     applyHighlight()
+})
+
+colorEditor.addEventListener('change', ()=> {
+    selectedColor()
 })
